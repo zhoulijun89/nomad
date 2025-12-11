ee8600be2f9b41fb41735a8d9596c814486c60b1
zhoulijun89 <zhoulijun89@gmail.com>
2025-12-11 19:47:17 +0800

    心跳间隔修改为0-3秒
    心跳间隔修改为0-3秒

--- client/client.go
+++ client/client.go
@@ -103,7 +103,7 @@ const (
 	// initialHeartbeatStagger is used to stagger the interval between
 	// starting and the initial heartbeat. After the initial heartbeat,
 	// we switch to using the TTL specified by the servers.
-	initialHeartbeatStagger = 10 * time.Second
+	initialHeartbeatStagger = 3 * time.Second
 
 	// nodeUpdateRetryIntv is how often the client checks for updates to the
 	// node attributes or meta map.
@@ -1831,16 +1831,18 @@ func (c *Client) registerAndHeartbeat() {
 				c.retryRegisterNode()
 				heartbeat = time.After(helper.RandomStagger(initialHeartbeatStagger))
 			} else {
-				intv := c.getHeartbeatRetryIntv(err)
+				//intv := c.getHeartbeatRetryIntv(err)
+				intv := helper.RandomStagger(initialHeartbeatStagger)
 				c.logger.Error("error heartbeating. retrying", "error", err, "period", intv)
 				heartbeat = time.After(intv)
-
+				
 				// If heartbeating fails, trigger Consul discovery
 				c.triggerDiscovery()
 			}
 		} else {
 			c.heartbeatLock.Lock()
-			heartbeat = time.After(c.heartbeatTTL)
+			// heartbeat = time.After(c.heartbeatTTL)
+			heartbeat = time.After(helper.RandomStagger(initialHeartbeatStagger))
 			c.heartbeatLock.Unlock()
 		}
 	}
